7023aad0013587d4f3e1d3a1fbfde2520f7951fc
Alexey <alexey6486@gmail.com>
2020-08-26 15:16:21 +0300

    header

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,12 @@ import {Route, Switch} from 'react-router-dom';
 import './App.css';
 import {ShopPage} from "./pages/shop/shop.component";
 import {Homepage} from "./pages/homepage/homepage";
+import { Header } from './components/header/header.component';
 
 export const App = () => {
     return (
         <div className="App">
+            <Header/>
             <Switch>
                 <Route exact path={'/'} component={Homepage}/>
                 <Route exact path={'/shop'} component={ShopPage}/>
---
+++ src/components/header/header.component.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import './header.styles.scss';
+
+export const Header = () => {
+
+    return (
+        <div className={'header'}>
+            <div className={'container'}>
+                <Link className={'link'} to={'/'}>
+                    Home
+                </Link>
+                <div className={'nav'}>
+                    <Link className={'link'} to={'/shop'}>
+                        Shop
+                    </Link>
+                    <Link className={'link'} to={'/'}>
+                        Contact
+                    </Link>
+                </div>
+            </div>
+        </div>
+    )
+}
