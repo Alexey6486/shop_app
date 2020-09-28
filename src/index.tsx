99cf44d21bddfaab4314dd4f99e2479421a3fc41
Alexey <alexey6486@gmail.com>
2020-09-28 12:04:04 +0300

    graphql(cors_issues)

--- src/index.tsx
+++ src/index.tsx
@@ -11,6 +11,7 @@ import {ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
 export const client: any = new ApolloClient({
     uri: 'https://crwn-clothing.com',
     cache: new InMemoryCache(),
+    credentials: 'same-origin',
 });
 
 // client.query({
